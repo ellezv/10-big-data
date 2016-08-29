47ae7e5736f02b12d02e1dbae2d96d4dd93f8334
Maelle Vance <maellevance@gmail.com>
2016-08-29 10:56:43 -0700

    follow along in class

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,11 +1,45 @@
 (function(module) {
-  var zip = {};
+  zip = {};
+  zip.topFive = [];
+  zip.neighborhoodTotals = {};
 
-  getData = function() {
+  var getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
       // TODO: start here!
+      zip.reformatted =
+      data.features.map(function(obj) {
+        return {
+          zipCode: obj.properties.zip,
+          neighborhood: obj.properties.neighborhood ? obj.properties.neighborhood.split(', ')[0] : null,
+          address: obj.properties.address ? obj.properties.address : null,
+          coordinates: {
+            lat: obj.geometry.coordinates[1],
+            lng: obj.geometry.coordinates[0]
+          }
+        };
+      })
+      .forEach(function(obj) {
+        var currentNeighborhood = obj.neighborhood;
+        zip.neighborhoodTotals[currentNeighborhood] ?
+        zip.neighborhoodTotals[currentNeighborhood] ++ :
+        zip.neighborhoodTotals[currentNeighborhood] = 1;
+      });
+      generateTopFive(zip.neighborhoodTotals);
+      zip.topFive.sort(function(currentNeighborhood, nextNeighborhood){
+        return nextNeighborhood.totalZips - currentNeighborhood.totalZips;
+      }).splice(5);
     });
   };
+
+  function generateTopFive(dictionary) {
+    for (var neighborhood in dictionary) {
+      zip.topFive.push({
+        neighborhood: neighborhood,
+        totalZips: dictionary[neighborhood]
+      });
+    }
+  }
+
 
   getData();
   module.zip = zip;
